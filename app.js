6b58562faf24c80fce3d620292dfe7d53ea74a81
emmam-png <emma.moloney@gmail.com>
2026-07-23 12:40:35 +0100

    Refactor chart creation with dynamic data input
    Updated the createChart function to accept dynamic data and labels from an external source. Added event listener for file input to read and process Excel data for charting.

--- app.js
+++ app.js
@@ -1,62 +1,129 @@
+let chart;
+
+
+function createChart(values){
+
 const ctx = document
 .getElementById("savingsChart");
 
 
-new Chart(ctx, {
+if(chart){
+    chart.destroy();
+}
+
+
+chart = new Chart(ctx, {
 
 type:"line",
 
 data:{
 
-
-labels:[
-"Jan",
-"Feb",
-"Mar",
-"Apr",
-"May",
-"Jun",
-"Jul"
-],
-
+labels:values.labels,
 
 datasets:[{
 
 label:"Savings",
 
-data:[
-12000,
-14000,
-15500,
-17000,
-19000,
-21500,
-24150
-],
-
+data:values.data,
 
 tension:.4
 
 }]
 
+}
 
-},
+});
 
-
-options:{
-
-responsive:true,
-
-plugins:{
-
-legend:{
-
-display:true
 
 }
 
+
+
+document
+.getElementById("excelFile")
+.addEventListener("change", function(e){
+
+
+const file=e.target.files[0];
+
+
+const reader=new FileReader();
+
+
+reader.onload=function(event){
+
+
+const workbook =
+XLSX.read(
+event.target.result,
+{
+type:"binary"
 }
+);
+
+
+
+const sheet =
+workbook.Sheets[
+workbook.SheetNames[0]
+];
+
+
+const data =
+XLSX.utils.sheet_to_json(sheet);
+
+
+
+console.log(data);
+
+
+
+let totalSavings=0;
+
+
+let dates=[];
+let amounts=[];
+
+
+
+data.forEach(row=>{
+
+
+if(row.Category==="Savings"){
+
+totalSavings += Number(row.Amount);
+
+dates.push(row.Date);
+
+amounts.push(totalSavings);
 
 }
+
+
+});
+
+
+
+document.querySelector(".card h2")
+.innerHTML =
+"£"+totalSavings;
+
+
+
+createChart({
+
+labels:dates,
+
+data:amounts
+
+});
+
+
+};
+
+
+
+reader.readAsBinaryString(file);
+
 
 });
